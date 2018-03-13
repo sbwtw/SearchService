e74c1c125d13d88d2ba6573a467f8c7099f6277f
石博文 <sbw@sbw.so>
2018-03-13 20:20:18 +0800

    add fuzzy search result storage struct

--- src/search_context.rs
+++ src/search_context.rs
@@ -1,6 +1,33 @@
 
 use pinyin::*;
 
+use std::cmp::{Eq, PartialEq};
+
+// start, len
+struct FuzzySearchResult {
+    start: i32,
+    len: i32,
+}
+
+impl FuzzySearchResult {
+    pub fn to_string<T: AsRef<str>>(&self, context: T) -> String {
+        let start = self.start as usize;
+        let end = (self.start + self.len) as usize;
+
+        context.as_ref()[start..end].to_string()
+    }
+}
+
+impl Eq for FuzzySearchResult {}
+
+impl PartialEq for FuzzySearchResult {
+    fn eq(&self, other: &Self) -> bool {
+        self.start == other.start &&
+        self.len == other.len
+    }
+}
+
+
 #[derive(Debug)]
 pub struct SearchContext {
     context: String,
@@ -22,6 +49,8 @@ impl SearchContext {
     pub fn set_context<T: AsRef<str>>(&mut self, context: T) {
         self.context = context.as_ref().to_string();
         self.pinyin_context = pinyin(context.as_ref(), &Args::new());
+
+        println!("{}\n{:?}", self.context, self.pinyin_context);
     }
 
     pub fn search<T: AsRef<str>>(&self, pattern: T) -> i32 {
@@ -43,6 +72,12 @@ impl SearchContext {
         r
     }
 
+    pub fn fuzzy_search<T: AsRef<str>>(&self, pattern: T) -> Vec<FuzzySearchResult> {
+        let r = vec![];
+
+        r
+    }
+
     pub fn search_pinyin<T: AsRef<str>>(&self, pattern: T) -> (i32, i32) {
 
         (3, 5)
@@ -50,5 +85,18 @@ impl SearchContext {
 
     pub fn search_pinyin_all<T: AsRef<str>>(&self, pattern: T) -> Vec<(i32, i32)> {
         unimplemented!()
+    }
+}
+
+#[cfg(test)]
+mod test {
+
+    use search_context::*;
+
+    #[test]
+    fn test_fuzzy_search_result() {
+        let r = FuzzySearchResult { start: 2, len: 3, };
+
+        assert_eq!(r.to_string("abcdef"), "cde".to_owned());
     }
 }
