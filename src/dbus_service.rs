358f26b5e6cde2e34c3ed68e0a2ddb30fb39a630
石博文 <sbw@sbw.so>
2018-03-13 19:28:11 +0800

    implement basic search

--- src/dbus_service.rs
+++ src/dbus_service.rs
@@ -116,24 +116,34 @@ fn create_context_interface(service: Arc<SearchService>) -> tree::Interface<MTFn
             .on_set(|i, m| {
                 let value: String = i.read()?;
                 let context: &Option<RwLock<SearchContext>> = m.path.get_data();
-                context.as_ref().map(move |x| x.write().unwrap().context = value);
+                context.as_ref().map(move |x| x.write().unwrap().set_context(value));
 
                 Ok(())
             })
             .on_get(|i, m| {
                 let context: &Option<RwLock<SearchContext>> = m.path.get_data();
-                i.append(context.as_ref().map(|x| x.read().unwrap().context.clone()).unwrap());
+                i.append(context.as_ref().map(|x| x.read().unwrap().context().to_owned()).unwrap());
 
                 Ok(())
             })
         )
+
+        .add_m(f.method("Search", (), |m| {
+            let pattern: &str = m.msg.read1()?;
+            let r = m.msg.method_return();
+            let context: &Option<RwLock<SearchContext>> = m.path.get_data();
+
+            Ok(vec![r.append1(context.as_ref().map(|x| x.read().unwrap().search(pattern)).unwrap())])
+        }).inarg::<String, _>("pattern").outarg::<i32, _>("occurs"))
+
         .add_m(f.method("SearchAll", (), |m| {
             let pattern: &str = m.msg.read1()?;
             let r = m.msg.method_return();
             let context: &Option<RwLock<SearchContext>> = m.path.get_data();
 
             Ok(vec![r.append1(context.as_ref().map(|x| x.read().unwrap().search_all(pattern)).unwrap())])
-        }).inarg::<String, _>("pattern").outarg::<Vec<(i32, i32)>, _>("occurs"))
+        }).inarg::<String, _>("pattern").outarg::<Vec<i32>, _>("occurs"))
+
         .add_m(f.method("SearchPinyin", (), |m| {
             let pattern: &str = m.msg.read1()?;
             let r = m.msg.method_return();
--- src/search_context.rs
+++ src/search_context.rs
@@ -3,45 +3,52 @@ use pinyin::*;
 
 #[derive(Debug)]
 pub struct SearchContext {
-    pub context: String,
-    pinyin_context: Option<String>,
+    context: String,
+    pinyin_context: Vec<Vec<String>>,
 }
 
 impl SearchContext {
     pub fn new() -> Self {
         SearchContext {
             context: String::new(),
-            pinyin_context: None,
+            pinyin_context: vec![],
         }
     }
 
-    pub fn search<T: AsRef<str>>(&self, pattern: T) -> (i32, i32) {
-        unimplemented!()
+    pub fn context(&self) -> &str {
+        &self.context
     }
 
-    pub fn search_all<T: AsRef<str>>(&self, pattern: T) -> Vec<(i32, i32)> {
-        println!("Searching {} ...", pattern.as_ref());
-        vec![(3, 5)]
+    pub fn set_context<T: AsRef<str>>(&mut self, context: T) {
+        self.context = context.as_ref().to_string();
+        self.pinyin_context = pinyin(context.as_ref(), &Args::new());
     }
 
-    pub fn search_pinyin<T: AsRef<str>>(&mut self, pattern: T) -> (i32, i32) {
+    pub fn search<T: AsRef<str>>(&self, pattern: T) -> i32 {
+        match self.context.find(pattern.as_ref()) {
+            Some(idx) => idx as i32,
+            None => -1,
+        }
+    }
 
-        println!("{:?}", self.pinyin_context());
+    pub fn search_all<T: AsRef<str>>(&self, pattern: T) -> Vec<i32> {
+        let mut r = vec![];
+        let mut i = 0;
+
+        while let Some(idx) = self.context[i..].find(pattern.as_ref()) {
+            r.push((idx + i) as i32);
+            i += idx + 1;
+        }
+
+        r
+    }
+
+    pub fn search_pinyin<T: AsRef<str>>(&self, pattern: T) -> (i32, i32) {
 
         (3, 5)
     }
 
     pub fn search_pinyin_all<T: AsRef<str>>(&self, pattern: T) -> Vec<(i32, i32)> {
         unimplemented!()
-    }
-
-    fn pinyin_context(&mut self) -> &str {
-        let ref c = self.context;
-
-        self.pinyin_context.get_or_insert_with(|| {
-            let s = pinyin(&c, &Args::new());
-            println!("{:?}", s);
-            "asd".to_owned()
-        })
     }
 }
